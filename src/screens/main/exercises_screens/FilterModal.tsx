93934ba6c5da1fb0426c385436c678fa0d1be63f
misha <mishalxv35@gmail.com>
2022-09-25 19:15:38 -0400

    positioned modal and added background opactity

--- src/screens/main/exercises_screens/FilterModal.tsx
+++ src/screens/main/exercises_screens/FilterModal.tsx
@@ -24,43 +24,56 @@ const FilterModal: React.FC<IFilterModalProps> = ({ navigation }) => {
   const { theme } = useTheme();
 
   return (
-    <Modal
-      animationType="fade"
-      visible={true}
-      onRequestClose={() => {
-        navigation.goBack();
-      }}
-      transparent
-    >
-      <View style={{ backgroundColor: theme.background }}>
-        <CustomText>Filter</CustomText>
-        <CustomText>Exercise Types</CustomText>
-        <View style={styles.buttons}>
-          {exercise_types.map((type, index) => {
-            return (
-              <View style={{ margin: 5 }} key={index}>
-                <ToggleButton toggled key={index} title={type} />
-              </View>
-            );
-          })}
-        </View>
-        <CustomText>Muscle Groups</CustomText>
-        <View style={styles.buttons}>
-          {exercise_muscle_groups.map((type, index) => {
-            return (
-              <View style={{ margin: 5 }} key={index}>
-                <ToggleButton toggled key={index} title={type} />
-              </View>
-            );
-          })}
-        </View>
-        <Button_C title="Close" onPress={() => navigation.goBack()} />
+    <View style={styles.container}>
+      <View style={{margin: 'auto'}}>
+        <Modal
+          animationType="fade"
+          visible={true}
+          onRequestClose={() => {
+            navigation.goBack();
+          }}
+          transparent
+        >
+          <View style={{margin: 'auto', backgroundColor: theme.background }}>
+            <CustomText>Filter</CustomText>
+            <CustomText>Exercise Types</CustomText>
+            <View style={styles.buttons}>
+              {exercise_types.map((type, index) => {
+                return (
+                  <View style={{ margin: 5 }} key={index}>
+                    <ToggleButton toggled key={index} title={type} />
+                  </View>
+                );
+              })}
+            </View>
+            <CustomText>Muscle Groups</CustomText>
+            <View style={styles.buttons}>
+              {exercise_muscle_groups.map((type, index) => {
+                return (
+                  <View style={{ margin: 5 }} key={index}>
+                    <ToggleButton toggled key={index} title={type} />
+                  </View>
+                );
+              })}
+            </View>
+            <Button_C title="Close" onPress={() => navigation.goBack()} />
+          </View>
+        </Modal>
       </View>
-    </Modal>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 25,
+    backgroundColor: "#111111EE",
+  },
   buttons: {
     flexWrap: "wrap",
     justifyContent: "flex-start",
